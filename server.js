978dda473e71c07317f763dfabf3cd52f9cf6a2e
rikysbd <rikysbd@gmail.com>
2025-08-06 22:50:49 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/create-payment-intent", async (req, res) => {
+  const { amount } = req.body;
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: "eur",
+    });
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Backend CositasCool activo 💖");
+});
+
+app.listen(3000, () => {
+  console.log("Servidor escuchando en puerto 3000");
+});
